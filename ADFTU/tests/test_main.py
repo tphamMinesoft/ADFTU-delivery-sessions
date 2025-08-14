9e12e0d4778cd3044b9f106398a85eaaede1b18d
tphamMinesoft <tpham@minesoft.com>
2025-08-14 12:20:42 +0100

    Added functions

--- ADFTU/tests/test_main.py
+++ ADFTU/tests/test_main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from functions.file_readers import read_csv
+from functions.data_merger import merge_df
 import pytest_cov
 '''
 As a user i want to be able to read the stock and sales files and have them as panda dataframes
@@ -13,8 +14,22 @@ def test_read_csv_type():
     # Act
     ouput = read_csv(filename)
     
+#as a user I want to be able to merge 2 datafame by a product_id field then subtract sales from stock
+def test_merge_data_column_count():
+    # Arrange
+    stock_filename = "data/test_stock.csv"
+    sales_filename = "data/test_sales.csv"
+    
+    stock_df = read_csv(stock_filename)
+    sales_df = read_csv(sales_filename)
+    
+    #Act
+    merged_df = merge_df(stock_df, sales_df)
+    
+    #Assert
+    
     # Assert
-    assert type(output) is expected_datatype
+    assert len(merged_df.columns)==11
 
 def test_read_csv_content_id():
     # Arrange
